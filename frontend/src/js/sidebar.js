b17f9ba2c5b73bd1186d0427ec87d958561c6876
Pedro <pedroliveiralima21@gmail.com>
2025-03-06 06:20:58 -0300

    feat: add menu mobile and menu desktop

--- frontend/src/js/sidebar.js
+++ frontend/src/js/sidebar.js
@@ -1,1 +1,7 @@
+const menuIcon = document.getElementById("menu-icon")
+const navLinks = document.getElementById("nav-links")
+const avatar = document.getElementById("avatar")
 
+menuIcon.addEventListener("click", () => {
+   navLinks.classList.toggle("show")
+})
