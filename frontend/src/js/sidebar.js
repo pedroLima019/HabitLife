6814ca4f474ff9b9a81b505f7669016541c6a7c4
Pedro <pedroliveiralima21@gmail.com>
2025-03-12 06:28:51 -0300

    Feat: create avatar-dropdown menu

--- frontend/src/js/sidebar.js
+++ frontend/src/js/sidebar.js
@@ -1,7 +1,12 @@
-const menuIcon = document.getElementById("menu-icon")
-const navLinks = document.getElementById("nav-links")
-const avatar = document.getElementById("avatar")
+const menuIcon = document.getElementById("menu-icon");
+const navLinks = document.getElementById("nav-links");
+const avatar = document.getElementById("avatar");
+const avatarDrop = document.getElementById("avatar-dropdown");
 
 menuIcon.addEventListener("click", () => {
-   navLinks.classList.toggle("show")
-})
+  navLinks.classList.toggle("show");
+});
+
+avatar.addEventListener("click", () => {
+  avatarDrop.classList.toggle("show");
+});
